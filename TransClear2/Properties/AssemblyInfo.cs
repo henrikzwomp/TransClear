39226c19782a55d6f425b69d9360620b665a1841
Henrik Persson <henrik.persson@intersolia.com>
2023-06-02 14:53:42 +0200

    Updated Assembly info

--- TransClear2/Properties/AssemblyInfo.cs
+++ TransClear2/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Lxf Thumbnail Handler")]
-[assembly: AssemblyDescription("TransClear Thumbnail Hander for Lxf-files.")]
+[assembly: AssemblyTitle("TransClear")]
+[assembly: AssemblyDescription("Thumbnail Hander for Lxf- and Io-files.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Henrik Persson")]
 [assembly: AssemblyProduct("TransClear")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
