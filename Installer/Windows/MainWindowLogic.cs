8bd2d696c29194c06d0c4e4c971851aab5099fe2
Henrik Persson <henrik.persson@intersolia.com>
2023-06-02 14:17:17 +0200

    Added logic for installing IoThumbnailHandler

--- Installer/Windows/MainWindowLogic.cs
+++ Installer/Windows/MainWindowLogic.cs
@@ -52,6 +52,11 @@ namespace Installer.Windows
             _handler.InstallExtension(_lxf_data);
             _handler.RegisterThumbnailHandler(_lxf_data);
             _handler.ApproveExtension(_lxf_data);
+
+            _handler.InstallExtension(_io_data);
+            _handler.RegisterThumbnailHandler(_io_data);
+            _handler.ApproveExtension(_io_data);
+
             Shell32dll.RefreshShell();
             MessageBox.Show("TransClear has been successfully installed. Click OK to exit.", "Success");
             Application.Current.Shutdown();
@@ -59,9 +64,14 @@ namespace Installer.Windows
 
         private void UninstallTransClear()
         {
-            _handler.UnapproveExtension(_lxf_data);
-            _handler.UnregisterServerAssociations(_lxf_data);
-            _handler.UninstallExtension(_lxf_data);
+            _handler.UnapproveExtension(_io_data);
+            _handler.UnregisterServerAssociations(_io_data);
+            _handler.UninstallExtension(_io_data);
+
+            _handler.UnapproveExtension(_io_data);
+            _handler.UnregisterServerAssociations(_io_data);
+            _handler.UninstallExtension(_io_data);
+
             Shell32dll.RefreshShell();
             MessageBox.Show("TransClear has been successfully uninstalled. Click OK to exit.", "Success");
             Application.Current.Shutdown();
@@ -69,21 +79,25 @@ namespace Installer.Windows
 
         private void CheckState()
         {
-            bool is_installed = _handler.IsInstalled(_lxf_data);
-            bool is_registed = _handler.IsRegistered(_lxf_data);
-            bool is_approved = _handler.IsApproved(_lxf_data);
+            bool lxf_is_installed = _handler.IsInstalled(_lxf_data);
+            bool lxf_is_registed = _handler.IsRegistered(_lxf_data);
+            bool lxf_is_approved = _handler.IsApproved(_lxf_data);
 
-            if (is_installed && is_registed && is_approved)
+            bool io_is_installed = _handler.IsInstalled(_lxf_data);
+            bool io_is_registed = _handler.IsRegistered(_lxf_data);
+            bool io_is_approved = _handler.IsApproved(_lxf_data);
+
+            if (lxf_is_installed && lxf_is_registed && lxf_is_approved && io_is_installed && io_is_registed && io_is_approved)
             {
-                CurrentStateMessage = "Current state: TransClear is fully installed";
+                CurrentStateMessage = "Current state: TransClear2 is fully installed";
             }
-            else if(is_installed || is_registed || is_approved)
+            else if(lxf_is_installed || lxf_is_registed || lxf_is_approved || io_is_installed || io_is_registed || io_is_approved)
             {
-                CurrentStateMessage = "Current state: TransClear is not fully installed";
+                CurrentStateMessage = "Current state: TransClear2 is not fully installed";
             }
             else
             {
-                CurrentStateMessage = "Current state: TransClear is not installed";
+                CurrentStateMessage = "Current state: TransClear2 is not installed";
             }
         }
     }
